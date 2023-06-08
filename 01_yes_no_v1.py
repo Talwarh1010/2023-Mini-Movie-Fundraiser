817f7cabc90057b2806492635ff87e431460b5b2
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2023-06-08 20:35:17 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,7 @@
+want_instructions = input("Do you want to read the instructions? ").lower()
+if want_instructions == "yes":
+    print("Instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("Please answer yes / no")
