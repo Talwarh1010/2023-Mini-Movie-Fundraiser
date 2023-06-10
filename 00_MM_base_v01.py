12130244ef75fab0214878349f729735b6b84cb8
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2023-06-10 22:05:45 +1200

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -1,4 +1,4 @@
-
+import pandas
 def not_blank(question):
     while True:
         response = input(question)
@@ -37,9 +37,23 @@ def string_checker(question, num_letters, valid_responses):
         print(f"Please choose {valid_responses[0]} or {valid_responses[1]}")
 
 
-MAX_TICKETS = 3
+def currency(x):
+    return "${:.2f}".format(x)
+
+
+MAX_TICKETS = 5
 tickets_sold = 0
-want_instructions = string_checker("Do you want to read the instructions? ", num_letters=0, valid_responses=["yes", "no"])
+all_names = []
+all_ticket_costs = []
+all_surcharge = []
+
+mini_movie_dict = {
+      "Name": all_names,
+      "Ticket Price": all_ticket_costs,
+      "Surcharge": all_surcharge
+}
+
+want_instructions = string_checker("Do you want to read the instructions? ", num_letters=1, valid_responses=["yes", "no"])
 
 if want_instructions == "yes" or want_instructions == "y":
     print("Instructions go here")
@@ -65,7 +79,38 @@ while tickets_sold < MAX_TICKETS:
     ticket_cost = round(calc_ticket_price(age),2)
     pay_method = string_checker("How would you like to pay: ", num_letters = 2, valid_responses = ["cash", "credit"])
     
+    if pay_method == "cash":
+        surcharge = 0
+    
+    else:
+        surcharge = ticket_cost * 0.05
+    
     tickets_sold += 1
+    
+    all_names.append(name)
+    all_ticket_costs.append(ticket_cost)
+    all_surcharge.append(surcharge)
+    
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index("Name")
+mini_movie_frame["Total"] = mini_movie_frame['Surcharge'] + mini_movie_frame['Ticket Price']
+mini_movie_frame["Profit"] = mini_movie_frame["Ticket Price"] -5
+total = mini_movie_frame['Total'].sum()
+profit = mini_movie_frame['Profit'].sum()
+add_dollars = ["Ticket Price", "Surcharge", "Total", "Profit"]
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+    
+print("---- Ticket Data ----")
+print()
+print(mini_movie_frame)
+print()
+print("---- Ticket Cost / Profit ----")
+print()
+print(f"Total Ticket Sales: ${total:.2f} ")
+print(f"Total Profit: ${profit:.2f}")
+
+
 if tickets_sold == MAX_TICKETS:
     print("Congratulations, you have sold all the tickets")
 else:
--- ZZ_sandpit.py
+++ ZZ_sandpit.py
@@ -1,19 +1,3 @@
+import pandas as pd
 
-
-def string_checker(question, num_letters, valid_responses):
-    while True:
-        response = input(question).lower()
-        for item in valid_responses:
-            if response == item[:num_letters] or response == item:
-                return item
-        print(f"Please choose {valid_responses[0]} or {valid_responses[1]}")
-    
-yes_no_list = ["yes", "no"]
-payment_list = ["cash", "credit"]
-for case in range(0,5):
-    want_instructions = string_checker("Do you want to see the instructions (y/n): ", num_letters = 1, valid_responses = yes_no_list)
-    print(f"You chose {want_instructions}")
-    
-for case in range(0,5):
-    pay_method = string_checker("How would you like to pay: ", num_letters = 2, valid_responses = payment_list)
-    print(f"You chose {pay_method}")
+print(pd.__version__)
